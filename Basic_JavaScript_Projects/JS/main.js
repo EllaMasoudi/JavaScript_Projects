72b75ada736e4c5d3c3b5480cd0bc029234c976f
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-07-16 13:02:44 -0700

    Update main.js

--- Basic_JavaScript_Projects/JS/main.js
+++ Basic_JavaScript_Projects/JS/main.js
@@ -1,5 +1,7 @@
 
 var A="Welcome" , B="Hi!"; //defining two variables
+var B = "This cup is unique " + "it is also a fragile cup!"
+var PinkCup ="From England", YellowCup ="From Russia", BlueCup = "From China";//multiple variables in one statement
 
 var  cup = "I have a blue cup."  ; //defining a variable and giving it a string value
 
@@ -8,4 +10,6 @@ var cup = cup.fontcolor("blue");//Using the fontcolor on string variable
 document.write(cup);//print the value of the cup 
 window.alert(A); //create pop-up alert with the value of the A
 document.write("\n", "I say: \"This is a text\""+ "\n", "-Elham" ,"\n","\n"); //print a quotations phrase  
-document.write(4+4);//print an expression
+document.write("\n", 4+4);//print an expression
+document.write( "\n", B);
+document.write("\n", YellowCup);
