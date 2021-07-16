e1f660266c036f035df0166ab2cf8e9faeb319f8
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-07-16 12:01:55 -0700

    JavaScript

---
+++ Basic_JavaScript_Projects/JS/main.js
@@ -0,0 +1,11 @@
+
+var A="Welcome" , B="Hi!"; //defining two variables
+
+var  cup = "I have a blue cup."  ; //defining a variable and giving it a string value
+
+var cup = cup.fontcolor("blue");//Using the fontcolor on string variable
+
+document.write(cup);//print the value of the cup 
+window.alert(A); //create pop-up alert with the value of the A
+document.write("\n", "I say: \"This is a text\""+ "\n", "-Elham" ,"\n","\n"); //print a quotations phrase  
+document.write(4+4);//print an expression
