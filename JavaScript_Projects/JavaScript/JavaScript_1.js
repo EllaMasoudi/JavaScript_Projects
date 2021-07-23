518a4e5f7cc7a60e1e3d99923ecea902cfe92d4d
EllaMasoudi <86387984+EllaMasoudi@users.noreply.github.com>
2021-07-23 13:16:09 -0700

    calculator

--- JavaScript_Projects/JavaScript/JavaScript_1.js
+++ JavaScript_Projects/JavaScript/JavaScript_1.js
@@ -23,4 +23,21 @@ function Food_Function(){
 
     }
     document.getElementById("Output").innerHTML = Food_Output;
+}//an example of switch statement, there are cases that are different conditions. if there is no match cases then defult will run. 
+
+
+function My_Cup(){
+    var A =document.getElementsByClassName("click");
+    A[1].innerHTML = "It is on the table! You found it!";
 }
+
+var C = document.getElementById("ID_Name");
+var ctx = C.getContext("2d");
+
+var grd =ctx.createLinearGradient(0, 0, 170,0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+ctx.fillStyle = grd;
+ctx.fillRect(0,0,500,250);
+ctx.font = "50px Arial";
+ctx.strokeText("Hello!" , 10,70);
